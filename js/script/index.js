bc3c09969fa26a22e1cec1dc1c05d67fda9741f4
Gina Tipa Herrera <gitipa.uxuic2@ucamp.utel.edu.mx>
2022-05-26 17:45:03 -0500

    Versión 3 - funcionando

--- js/script/index.js
+++ js/script/index.js
@@ -1,10 +1,14 @@
-items = []
-
-let mostrarItems = (itemsRecibidos) => {
+let mostrarItems = () => {
+    let itemsLS = JSON.parse(localStorage.getItem('itemsls'))
+    if (itemsLS === null) {
+        console.log('entro');
+        localStorage.setItem('itemsls', JSON.stringify([]));
+        itemsLS = []
+    }
     document.getElementsByTagName("tbody")[0].innerHTML = '';
-    itemsRecibidos.forEach((element, index) => {
-        let texth1 = document.createElement("tr");
-        texth1.innerHTML =
+    itemsLS.forEach((element, index) => {
+        let action1 = document.createElement("tr");
+        action1.innerHTML =
             `<td scope="row">${index + 1}</td>
             <td>${element.nameObj}</td>
             <td>${element.numberObj}</td>
@@ -12,50 +16,63 @@ let mostrarItems = (itemsRecibidos) => {
                 <button type="button" class="btn btn-success" onclick="editarFormulario(${index})">Editar</button>
                 <button type="button" class="btn btn-warning" onclick="eliminarRegistro(${index})">Eliminar</button>
             </td>`;
-            document.getElementsByTagName("tbody")[0].appendChild(texth1);
+        document.getElementsByTagName("tbody")[0].appendChild(action1);
     });
 }
 
 let eliminarRegistro = (parIndex) => {
-    items = items.filter((item, index) => index !== parIndex);
-    mostrarItems(items);
+    itemsAlmacenados = JSON.parse(localStorage.getItem('itemsls'))
+    itemsAlmacenados = itemsAlmacenados.filter((item, index) => index !== parIndex);
+    localStorage.setItem('itemsls', JSON.stringify(itemsAlmacenados));
+    mostrarItems();
 }
 
 let editarFormulario = (parIndex) => {
+    itemsAlmacenados = JSON.parse(localStorage.getItem('itemsls'))
     document.getElementById('position').value = parIndex;
-    document.getElementById('nameObj').value = items[parIndex].nameObj;
-    document.getElementById('numberObj').value = items[parIndex].numberObj;
+    document.getElementById('nameObj').value = itemsAlmacenados[parIndex].nameObj;
+    document.getElementById('numberObj').value = itemsAlmacenados[parIndex].numberObj;
 }
 
 let actualizarRegistro = () => {
     let position = document.getElementById('position').value;
     let nameObj = document.getElementById('nameObj').value;
     let numberObj = document.getElementById('numberObj').value;
-
     if (position == '' || isNaN(position)) {
         alert('Debe seleccionar un registro de la lista');
         return false;
     }
-
-    items[position] = {
+    itemsAlmacenados = JSON.parse(localStorage.getItem('itemsls'));
+    itemsAlmacenados[position] = {
         nameObj: nameObj,
         numberObj: numberObj
     }
     document.getElementById('position').value = '';
     document.getElementById('nameObj').value = '';
     document.getElementById('numberObj').value = '';
-    mostrarItems(items);
+    localStorage.setItem('itemsls', JSON.stringify(itemsAlmacenados));
+    mostrarItems();
 }
 
 let agregarRegistro = () => {
-    console.log(`Agregar`);
+    itemsLS = JSON.parse(localStorage.getItem('itemsls'));
     const nuevoItem = {
         nameObj: document.getElementById('nameObj').value,
         numberObj: document.getElementById('numberObj').value
     }
     document.getElementById('nameObj').value = '';
     document.getElementById('numberObj').value = '';
-    items.push(nuevoItem);
-    mostrarItems(items);
+    itemsLS.push(nuevoItem);
+    localStorage.setItem('itemsls', JSON.stringify(itemsLS));
+    mostrarItems();
 }
-mostrarItems(items);
+
+let clearCache = () => {
+    localStorage.clear();
+    if (confirm('Se borran la lista de manera definitva')) {
+        } else {
+        }
+    mostrarItems();
+}
+
+mostrarItems();
